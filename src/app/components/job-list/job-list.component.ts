6bb12d34ec6e86880711f6d04920d32e60b60ee0
LIN Hongxiang :-) <1270610477@qq.com>
2025-06-01 23:13:53 +0200

    Job list updates

--- src/app/components/job-list/job-list.component.ts
+++ src/app/components/job-list/job-list.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { JobService, Job } from '../../services/job.service';
 
 @Component({
   selector: 'app-job-list',
-  template: `<div *ngFor="let job of jobs">{{ job.title }}</div>`,
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './job-list.component.html',
+  styleUrls: ['./job-list.component.scss']
 })
 export class JobListComponent implements OnInit {
   jobs: Job[] = [];
@@ -11,9 +15,11 @@ export class JobListComponent implements OnInit {
   constructor(private jobService: JobService) {}
 
   ngOnInit(): void {
-    this.jobService.getJobs().subscribe(data => {
-      this.jobs = data;
-      console.log('Jobs:', data);
+    this.jobService.getJobs().subscribe({
+      next: (data: Job[]) => (this.jobs = data),
+      error: (err: any) => console.error('Failed to load jobs:', err),
     });
   }
+
+
 }
